18aca915dd798fcd48feb8fa3936663f125d7c61
Sliderer <slidedru8gs@mail.ru>
2024-03-21 18:34:43 +0300

    feat cards to select: change format

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_ADDRESS = 'http://192.168.0.100:8090'
-export const SERVER_WEB_SOCKET_ADDRESS = 'http://192.168.0.100:8090/bang'
+export const SERVER_ADDRESS = 'http://127.0.0.1:8090'
+export const SERVER_WEB_SOCKET_ADDRESS = 'http://127.0.0.1:8090/bang'
--- src/ui/orgnisms/CardSelectionPanel.tsx
+++ src/ui/orgnisms/CardSelectionPanel.tsx
@@ -1,10 +1,17 @@
 import {CardForSelection} from "../moleculas/CardForSelection";
 import {PlayingCard} from "../../models/PlayingCard";
+import { CenterDiv } from "../../views/HomeView";
 
 export const CardSelectionPanel = ({cards, onSelect} : {cards: PlayingCard[], onSelect: Function}) => {
-    return <>
-        {cards.map((card, index) => {
-            return <CardForSelection card={card} index={index} onSelect={onSelect}/>
-        })}
-    </>
+    return <div style={{marginTop: 150}}>
+        <CenterDiv>
+            <h1>Выберите карту</h1>
+        </CenterDiv>
+        
+        <CenterDiv >
+            {cards.map((card, index) => {
+                return <CardForSelection card={card} index={index} onSelect={onSelect}/>
+            })}
+        </CenterDiv>
+    </div>
 }
